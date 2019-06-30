246872070a9bb71e725826a0bfccced697a89955
showchan1109 <showchan1109@gmail.com>
2019-06-30 16:08:26 +0900

    Create new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root to: 'messages#index'
+  resources :users, only: [:edit, :update]
+>>>>>>> Stashed changes
 end
